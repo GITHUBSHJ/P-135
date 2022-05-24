db791ebcb8a40f3fda3aac26055a3e6e6beb049e
SWASTIK SIBAM <89509745+GITHUBSHJ@users.noreply.github.com>
2022-05-24 23:48:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//**function preload(){} function setup(){} function draw(){} */
+console.log(
+	"%cMADE BY - SWASTIK SIBAM NAYAK",
+	"color: white; background:linear-gradient(#E66465, #9198E5); padding: 1.2em; border-radius: 6px;"
+ );
+
+console.log("NOTHING TO SAY BYE!!");
